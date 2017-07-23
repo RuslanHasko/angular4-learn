ff0cd11c48d0e4fc0ed1d4a4fed0b9e60dab8edd
Ruslan Hasko <ruslan.hasko@kindgeek.com>
2017-07-23 22:44:44 +0300

    Lecture 25 is complete

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline'
 }
